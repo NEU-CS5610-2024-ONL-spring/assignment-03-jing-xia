ef583e51fba790a9030e9edd692dae9c6150e7f4
JingXia <xia.jin@northeastern.edu>
2024-04-20 20:45:09 -0700

    Compelete three unit tests for city, detailHeader, profile components

--- client/src/test/setupTests.test.js
+++ client/src/test/setupTests.test.js
@@ -5,26 +5,54 @@
 import '@testing-library/jest-dom';
 import { setupServer } from 'msw/node';
 import React from 'react';
-import {render, fireEvent, screen} from '@testing-library/react';
+import {render, fireEvent, screen, waitFor} from '@testing-library/react';
 import { handlers } from './testServerHandlers';
-import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import City from '../components/City/City';
 import DetailHeader from '../components/Detail/DetailHeader';
 import { AuthTokenProvider } from '../AuthTokenContext';
-
-// global.TextEncoder = TextEncoder;
-
-// const url = "http://localhost:8000";
+import UserProfile from '../components/UserProfile/UserProfile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const server = setupServer(...handlers);
 
-beforeAll(() => {server.listen();});
-afterEach(() => server.resetHandlers());
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+jest.mock("@auth0/auth0-react");
+
+const mockGetToken = () => {
+  return "test token";
+};
+
+beforeAll(() => {
+  jest.mocked(useAuth0, true).mockReturnValue({
+    isAuthenticated: true,
+    getAccessTokenSilently: mockGetToken,
+    logout: jest.fn(),
+  });
+  server.listen();
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+  server.resetHandlers()
+});
+
 afterAll(() => server.close());
-// jest.spyOn(window, "close").mockImplementation(jest.fn());
 
 test("test if city component could display city name and weather info correctly", async () => {
-  // jest.mock('@auth0/auth0-spa-js');
   render(
     <MemoryRouter>
       <City 
@@ -66,8 +94,23 @@ test("test if detail header could load city and subscribe status correctly", asy
         country: "US",
         state: "California",
       }}/>
-    </AuthTokenProvider>
-  )
+    </AuthTokenProvider>)
   expect(screen.getByTestId("detail-header")).toHaveTextContent("Los Angeles, California, US");
   expect(screen.getByTestId("detail-header")).toHaveTextContent("Subscribe");
+});
+
+test("load profile page", async () => {
+    render(
+      <AuthTokenProvider>
+          <MemoryRouter>
+            <UserProfile />
+          </MemoryRouter>
+      </AuthTokenProvider>);
+  await waitFor(() => expect(screen.getByTestId("profile-content")).toHaveTextContent("test@northeastern.edu"));
+  expect(screen.getByTestId("profile-subscribed-city-list")).toHaveTextContent("Oakland");
+  expect(screen.getByTestId("profile-subscribed-city-list")).toHaveTextContent("London");
+  fireEvent.click(screen.getByText("Settings"));
+  expect(screen.getByTestId("profile-settings")).toHaveTextContent("Access to my locationEnable");
+  fireEvent.click(screen.getByText("Auth Debugger"));
+  expect(screen.getByTestId("profile-auth-debugger")).toHaveTextContent("Tokentest token");
 });
